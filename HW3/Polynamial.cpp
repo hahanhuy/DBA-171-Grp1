57c1feeb10e99a0921c8f4a58a7e187ea977fa52
hahanhuy <huyha1711@gmail.com>
2017-10-05 23:14:45 +0700

    Đã tách dc từng hệ số

--- HW3/Polynamial.cpp
+++ HW3/Polynamial.cpp
@@ -1,7 +1,3 @@
-// Polynamial.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -15,21 +11,16 @@ struct Term
 	int exponent;
 };
 
-class Polynamial {
+class Polynomial {
 private:
 	LinkedList<Term> *value;
 
 };
 
-
-
-int main()
-{
-	string s;
-	char tmp[20];
-	cout << "Enter polynamial: ";
-	cin >> s;
+void addelement(string s){
+	//LinkedList<Term> list;
 	int i = 0;
+	char tmp[20];
 	while (s[i] != '\0') {
 		int j = i;
 		do {
@@ -37,10 +28,36 @@ int main()
 			j++;
 		} while (s[j] != '+' && s[j] != '-' && s[j] != '\0');
 		tmp[j - i] = '\0';
-		string s1(tmp);
-		cout << s1 << " ";
+
+		istringstream iss(tmp);
+		double coefficient;
+		int exponent;
+		char x;
+		iss >> coefficient;
+		if (iss.eof())
+			exponent = 0;
+		else{
+			iss >> x >> x;
+			if (iss.eof())
+				exponent = 1;
+			else
+				iss >> exponent;
+		}
+		cout << coefficient << " " << exponent << endl;
 		i = j;
 	}
+
+}
+
+int main(){
+	string s;
+	
+	//cout << "Enter polynamial: ";
+	//cin >> s;
+
+	s = "5.2x^2-3x-4.213123213";
+	addelement(s);
+
 
 	system("pause");
     return 0;
