1496e4b119e9689f70728888e7d4d0d88648fa78
hahanhuy <huyha1711@gmail.com>
2017-10-06 13:59:06 +0700

    xong cái addelement
    add mấy cái phần tử của đa thức vô dc 1 cái list.
    những phần tử mà không tồn tại thì dc add với hệ số bằng 0.
    chưa có sort dc.

--- HW3/Polynamial.cpp
+++ HW3/Polynamial.cpp
@@ -18,25 +18,26 @@ private:
 
 };
 
-//void PrintList(LinkedList<Term> list){
-//	Node<Term> * temp = list.getHead();
-//	while (temp != NULL){
-//		cout << temp->data.coefficient << " " << temp->data.exponent << endl;
-//		temp = temp->next;
-//	}
-//}
+void PrintList(LinkedList<Term>* list){
+	Node<Term> * temp = list->getHead();
+	while (temp != NULL){
+		cout << temp->data.coefficient << " " << temp->data.exponent << endl;
+		temp = temp->next;
+	}
+}
 
-void addelement(string s){
-//	LinkedList<Term> list;
-	
+LinkedList<Term>* addelement(string s){
+	LinkedList<Term>* list = new LinkedList<Term>();
 	int i = 0;
-	char tmp[20];
+	char tmp[50];
+
 	while (s[i] != '\0') {
 		int j = i;
 		do {
 			tmp[j - i] = s[j];
 			j++;
 		} while (s[j] != '+' && s[j] != '-' && s[j] != '\0');
+		
 		tmp[j - i] = '\0';
 
 		istringstream iss(tmp);
@@ -53,22 +54,37 @@ void addelement(string s){
 			else
 				iss >> value.exponent;
 		}
-		//list.InsertFirst(value);
-		cout << value.coefficient << " " << value.exponent << endl;
+		list->InsertLast(value);
 		i = j;
-		
 	}
-	//PrintList(list);
+	
+	int degree = list->getHead()->data.exponent;
+	Node<Term>* monomial = list->getHead();
+	for (int i = 0; i < degree + 1; i++){
+		int current_expo = monomial->data.exponent;
+		if (current_expo != (degree - i)){
+			Term temp_monomial;
+			temp_monomial.coefficient = 0;
+			temp_monomial.exponent = degree - i;
+			list->InsertItem(temp_monomial, i);
+		}
+		else
+			monomial = monomial->next;
+	}
+
+	PrintList(list);
+	return list;
 }
 
 int main(){
 	string s;
-	LinkedList<Term> list;
+	LinkedList<Term>* list = new LinkedList<Term>();
 	//cout << "Enter polynamial: ";
 	//cin >> s;
 
-	s = "5.2x^2-3x-4.213123213";
-	addelement(s);
+	s = "5.2x^4-3x-4.213123213";
+	cout << s << endl << endl;
+	list = addelement(s);
 	
 
 	system("pause");
