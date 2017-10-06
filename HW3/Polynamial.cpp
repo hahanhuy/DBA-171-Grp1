64f5ae62be77abdd8c7d86ed16a9ba5e5fa09978
Victor <32546050+VictorNM@users.noreply.github.com>
2017-10-06 19:35:53 +0700

    Add files via upload

--- HW3/Polynamial.cpp
+++ HW3/Polynamial.cpp
@@ -1,6 +1,11 @@
+// Polynamial.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
 #include "LinkedList.h"
 
 using namespace std;
@@ -9,20 +14,24 @@ struct Term
 {
 	double coefficient;
 	int exponent;
-	
+
 };
 
 class Polynomial {
 private:
-	LinkedList<Term> *list = new LinkedList<Term>();
+	LinkedList<Term> *list;
+
 public:
+	Polynomial() {
+		list = new LinkedList<Term>;
+	}
 	void create(string s);
 	void print();
 	Polynomial* add(Polynomial* P);
 	Polynomial* sub(Polynomial* P);
 };
 
-void Polynomial::create(string s){
+void Polynomial::create(string s) {
 	int i = 0;
 	char tmp[50];
 
@@ -42,7 +51,7 @@ void Polynomial::create(string s){
 		iss >> value.coefficient;
 		if (iss.eof())
 			value.exponent = 0;
-		else{
+		else {
 			iss >> x >> x;
 			if (iss.eof())
 				value.exponent = 1;
@@ -55,16 +64,16 @@ void Polynomial::create(string s){
 
 	int degree = list->getHead()->data.exponent;
 	Node<Term>* monomial = list->getHead();
-	for (int i = 0; i < degree + 1; i++){
-		if (monomial == NULL){
+	for (int i = 0; i < degree + 1; i++) {
+		if (monomial == NULL) {
 			Term temp_monomial;
 			temp_monomial.coefficient = 0;
 			temp_monomial.exponent = degree - i;
 			list->InsertItem(temp_monomial, i);
 		}
-		else{
+		else {
 			int current_expo = monomial->data.exponent;
-			if (current_expo != (degree - i)){
+			if (current_expo != (degree - i)) {
 				Term temp_monomial;
 				temp_monomial.coefficient = 0;
 				temp_monomial.exponent = degree - i;
@@ -77,15 +86,15 @@ void Polynomial::create(string s){
 	}
 }
 
-void Polynomial::print(){
+void Polynomial::print() {
 	Node<Term> * temp = list->getHead();
-	while (temp != NULL){
+	while (temp != NULL) {
 		cout << temp->data.coefficient << " " << temp->data.exponent << endl;
 		temp = temp->next;
 	}
 }
 
-Polynomial* Polynomial::add(Polynomial* P){
+Polynomial* Polynomial::add(Polynomial* P) {
 	Polynomial* result = new Polynomial();
 	int thisdegree = this->list->getHead()->data.exponent;
 	int thatdegree = P->list->getHead()->data.exponent;
@@ -94,23 +103,23 @@ Polynomial* Polynomial::add(Polynomial* P){
 	node1 = this->list->getHead();
 	node2 = P->list->getHead();
 
-	for (int i = 0; i < length; i++){
+	for (int i = 0; i < length; i++) {
 		Term temp;
 		int expo1 = node1->data.exponent;
 		int expo2 = node2->data.exponent;
-		
-		if (expo1 > expo2){
+
+		if (expo1 > expo2) {
 			temp.coefficient = node1->data.coefficient;
 			temp.exponent = node1->data.exponent;
 			node1 = node1->next;
 		}
-		else{
-			if (expo2 > expo1){
+		else {
+			if (expo2 > expo1) {
 				temp.coefficient = node2->data.coefficient;
 				temp.exponent = node2->data.exponent;
 				node2 = node2->next;
 			}
-			else{
+			else {
 				temp.coefficient = node1->data.coefficient + node2->data.coefficient;
 				temp.exponent = node1->data.exponent;
 				node1 = node1->next;
@@ -122,7 +131,7 @@ Polynomial* Polynomial::add(Polynomial* P){
 	return result;
 }
 
-Polynomial* Polynomial::sub(Polynomial* P){
+Polynomial* Polynomial::sub(Polynomial* P) {
 	Polynomial* result = new Polynomial();
 	int thisdegree = this->list->getHead()->data.exponent;
 	int thatdegree = P->list->getHead()->data.exponent;
@@ -131,23 +140,23 @@ Polynomial* Polynomial::sub(Polynomial* P){
 	node1 = this->list->getHead();
 	node2 = P->list->getHead();
 
-	for (int i = 0; i < length; i++){
+	for (int i = 0; i < length; i++) {
 		Term temp;
 		int expo1 = node1->data.exponent;
 		int expo2 = node2->data.exponent;
 
-		if (expo1 > expo2){
+		if (expo1 > expo2) {
 			temp.coefficient = node1->data.coefficient;
 			temp.exponent = node1->data.exponent;
 			node1 = node1->next;
 		}
-		else{
-			if (expo2 > expo1){
+		else {
+			if (expo2 > expo1) {
 				temp.coefficient = -node2->data.coefficient;
 				temp.exponent = node2->data.exponent;
 				node2 = node2->next;
 			}
-			else{
+			else {
 				temp.coefficient = node1->data.coefficient - node2->data.coefficient;
 				temp.exponent = node1->data.exponent;
 				node1 = node1->next;
@@ -159,35 +168,118 @@ Polynomial* Polynomial::sub(Polynomial* P){
 	return result;
 }
 
-///////////////----------------//////////////////////////
-int main(){
+int main() {
+	const int NUM_POLY = 20;
 	string s;
+	string option;
+	int polyIdx = 0;
+	int polyListSize = 0;
 
-	//cout << "Enter polynamial: ";
-	//cin >> s;
+	Polynomial* polyList[NUM_POLY];
 
-	Polynomial *P1 = new Polynomial();
-	Polynomial *P2 = new Polynomial();
-	Polynomial *result = new Polynomial();
+	// input polynomial
+	do {
+		cout << "Enter polynamial (a1x^n1+a2x^n2+...): ";
+		getline(cin, s);
+
+		Polynomial *newPoly = new Polynomial();
+
+		// cannot allocate memory
+		if (newPoly == NULL) {
+			cout << "Memory is full!" << endl;
+			system("pause");
+			return -1;
+		}
+
+		newPoly->create(s);
+		polyList[polyIdx] = newPoly;
+		polyListSize++;
+
+		do {
+			cout << "Do you want to continue (y/n): ";
+			getline(cin, option);
+		} while (option != "y" && option != "n");
+
+	} while (option != "n");
 	
+	cout << "Stop input polynomial." << endl;
 
-	s = "5.2x^4-3x";
-	cout << "poly1: " << s << endl << endl;
-	P1->create(s);
-	//P1->print();
-	s = "5.2x^7-3x-6969";
-	cout << "poly2: " << s << endl << endl;
-	P2->create(s);
-	//P2->print();
-	result = P1->add(P2);
-	result->print();
+	// calculate
+	do {
+		char c;
+		string expr[3];
+		int i = 0;
+		string tmp;
+		istringstream iss(s);
+		int leftIdx = 0, rightIdx = 0;
+		double xValue = 0;
+		cout << "Perform math operations: (Pi add\\sub\\mul\\div Pj || Pj cal n)" << endl;
+		getline(cin, s);
 
-	cout << "SUB:\n";
+		// split string and store to expr
+		iss.str(s);
+		while (getline(iss, tmp, ' ')) expr[i++] = tmp;
+		iss.clear();
 
-	result = P1->sub(P2);
-	result->print();
+		// get left index
+		iss.str(expr[0]);
+		iss >> c >> leftIdx;
+		iss.clear();
+		if (leftIdx < 0 || leftIdx >= polyListSize) {
+			cout << "Index is out of bound" << endl;
+			continue;
+		}
+
+		// code for cal
+		// leftIdx - 1, rightIdx - 1 store the index of polynomial in polyList
+		// xValue store the value of x for 'cal'
+		if (expr[1] == "cal") {
+
+			// get value of x
+			iss.str(expr[2]);
+			iss >> xValue;
+			iss.clear();
+
+			// cal here
+		}
+		// code for other operations
+		else {
+			// get right index
+			iss.str(expr[2]);
+			iss >> c >> rightIdx;
+			iss.clear();
+			if (rightIdx < 0 || rightIdx >= polyListSize) {
+				cout << "Index is out of bound" << endl;
+				continue;
+			}
+			if (expr[1] == "add") {
+				// add here
+			}
+			else if (expr[1] == "sub") {
+				// sub here
+			}
+			else if (expr[1] == "mul") {
+				// mul here
+			}
+			else if (expr[1] == "div") {
+				// div here
+			}
+			else {
+				cout << "Error operator" << endl;
+				continue;
+			}
+		}
+		
+		cout << leftIdx << " " << expr[1] << " " << rightIdx << " " << xValue << endl;
+
+		do {
+			cout << "Do you want to continue (y/n): ";
+			getline(cin, option);
+		} while (option != "y" && option != "n");
+	} while (option != "n");
+
 
 	system("pause");
-    return 0;
+	return 0;
 }
 
